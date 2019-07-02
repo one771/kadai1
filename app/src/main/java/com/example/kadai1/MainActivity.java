e440ca8e5080b5c624be16f1a99a1bfa6bcd30ee
moriya <moriya@i-hearts.jp>
2019-07-02 17:29:56 +0900

    a

--- app/src/main/java/com/example/kadai1/MainActivity.java
+++ app/src/main/java/com/example/kadai1/MainActivity.java
@@ -28,16 +28,16 @@ public class MainActivity extends AppCompatActivity {
         // ボタンを取得　
         button.setOnClickListener(new View.OnClickListener() {
          // クリック時にメッセージを表示
-            private int count = 0;
+            private int ct = 0;
 
 
 
             @Override
             public void onClick(View v) {
 
-                count++;
+                ct++;
 
-                if(count == 15) {
+                if(ct == 15) {
                     //ボタンの実行回数
                     tv.setVisibility(View.VISIBLE);
                 }
